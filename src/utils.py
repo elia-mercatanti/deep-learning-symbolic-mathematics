c4828d50aa7bc3ab30a8ece6957eb8798939d994
Elia Mercatanti <eliamercatanti@gmail.com>
2021-06-28 18:10:57 +0200

    Restore original signals

--- src/utils.py
+++ src/utils.py
@@ -142,12 +142,12 @@ def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
 
         def _handle_timeout(repeat_id, signum, frame):
             # logger.warning(f"Catched the signal ({repeat_id}) Setting signal handler {repeat_id + 1}")
-            signal.signal(signal.SIGTERM, partial(_handle_timeout, repeat_id + 1))
+            signal.signal(signal.SIGALRM, partial(_handle_timeout, repeat_id + 1))
             signal.alarm(seconds)
             raise TimeoutErrorException(error_message)
 
         def wrapper(*args, **kwargs):
-            old_signal = signal.signal(signal.SIGTERM, partial(_handle_timeout, 0))
+            old_signal = signal.signal(signal.SIGALRM, partial(_handle_timeout, 0))
             old_time_left = signal.alarm(seconds)
             assert type(old_time_left) is int and old_time_left >= 0
             if 0 < old_time_left < seconds:  # do not exceed previous timer
@@ -160,7 +160,7 @@ def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
                     signal.alarm(0)
                 else:
                     sub = time.time() - start_time
-                    signal.signal(signal.SIGTERM, old_signal)
+                    signal.signal(signal.SIGALRM, old_signal)
                     signal.alarm(max(0, math.ceil(old_time_left - sub)))
             return result
 
